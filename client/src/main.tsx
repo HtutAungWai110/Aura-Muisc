495d4ed1052eacc6bf0eacc14212dfce28748980
HtutAungWai110 <lmaohtutaungwai@gmail.com>
2026-07-18 14:31:16 +0630

    Settings page added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import TanstackProvider from "./lib/QueryClientProvider.tsx";
+import ThemeProvider from "./components/ThemeProvider";
 
 createRoot(document.getElementById("root")!).render(
-  <TanstackProvider>
-    <App />
-  </TanstackProvider>,
+  <ThemeProvider>
+    <TanstackProvider>
+      <App />
+    </TanstackProvider>
+  </ThemeProvider>,
 );
