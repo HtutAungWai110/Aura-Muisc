a68ea254aa5a5810df512324d6986ddc908b263f
HtutAungWai110 <lmaohtutaungwai@gmail.com>
2026-07-23 21:30:01 +0630

    lee

---
+++ server/src/seedtest/seedPlaylist.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import Playlist from "../models/playlist.js"
+
+async function seedTestUser() {
+  try {
+    // 1. Connect to your database
+    await mongoose.connect("mongodb://localhost:27017/AuraMusic");
+    console.log("Connected to MongoDB successfully!");
+
+    const playlists = [
+      {
+        title: "my playlist",
+        userId: "6a11557f819a3493e4a9efef",
+      },
+      {
+        title: "my playlist",
+        userId: "6a11557f819a3493e4a9efef",
+      },
+      {
+        title: "my playlist",
+        userId: "6a11557f819a3493e4a9efef",
+      },
+      {
+        title: "my playlist",
+        userId: "6a11557f819a3493e4a9efef",
+      },
+      {
+        title: "my playlist",
+        userId: "6a11557f819a3493e4a9efef",
+      },
+      {
+        title: "my playlist",
+        userId: "6a11557f819a3493e4a9efef",
+      },
+      {
+        title: "my playlist",
+        userId: "6a11557f819a3493e4a9efef",
+      }
+    ]
+
+    for (const playlist in playlists) {
+      const newPlaylist = new Playlist({
+        title: playlists[playlist].title,
+        userId: playlists[playlist].userId,
+      });
+      await newPlaylist.save();
+    }
+  } catch (error) {
+    console.error("Error inserting test user:", error);
+  } finally {
+    // Clean up connection
+    await mongoose.disconnect();
+  }
+}
+
+seedTestUser();
